9b5fc71515d974ec817d37b46f5118362e6f437a
Paul Kippes <kippesp@gmail.com>
2017-06-04 23:15:22 -0700

    Make explosions

--- SpaceShooter/Assets/scripts/DestroyByContact.cs
+++ SpaceShooter/Assets/scripts/DestroyByContact.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
     public GameObject explosion;
+    public GameObject playerExplosion;
 
 
     void OnTriggerEnter(Collider other)
@@ -12,6 +13,9 @@ public class DestroyByContact : MonoBehaviour {
             return;
 
         Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player") {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
 
         Destroy(other.gameObject);
         Destroy(gameObject);
