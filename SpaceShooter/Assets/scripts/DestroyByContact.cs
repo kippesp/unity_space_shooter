80615bf6966361978b80906c46138a7934ef35e0
Paul Kippes <kippesp@gmail.com>
2017-06-04 22:47:14 -0700

    Added astroid

---
+++ SpaceShooter/Assets/scripts/DestroyByContact.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "bondary")
+            return;
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
