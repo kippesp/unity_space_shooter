1c7dd6a8b6bfbc1fd9c119c77f0c4aeb8becd17e
Paul Kippes <kippesp@gmail.com>
2017-06-04 23:00:23 -0700

    hummmm

--- SpaceShooter/Assets/scripts/DestroyByContact.cs
+++ SpaceShooter/Assets/scripts/DestroyByContact.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
+    public GameObject explosion;
+
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "bondary")
             return;
+
+        Instantiate(explosion, transform.position, transform.rotation);
 
         Destroy(other.gameObject);
         Destroy(gameObject);
