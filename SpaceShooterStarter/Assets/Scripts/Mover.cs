4981d830ec39bdb5359e0b18b61a17abdc1eff54
Paul Kippes <kippesp@gmail.com>
2017-06-19 15:57:58 -0700

    Add stubbed Mover file

---
+++ SpaceShooterStarter/Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    private Rigidbody rb;
+    private GameController gc;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        GameObject go = GameObject.FindWithTag("GameController");
+        gc = go.GetComponent<GameController>();
+    }
+}
