abd0ba6592c21f87d430ec6ab0c927b72ba34880
spillow <scott.pillow@gmail.com>
2017-06-18 20:56:58 -0700

    Feel free to revert this.  Different controls to spin and boost around.

--- SpaceShooter/Assets/scripts/PlayerController.cs
+++ SpaceShooter/Assets/scripts/PlayerController.cs
@@ -57,13 +57,21 @@ public class PlayerController : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed;
+        //rb.velocity = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed;
+        //rb.velocity = transform.forward * moveVertical * speed;
+
+        if (moveVertical > 0.0f)
+            rb.AddForce(transform.forward * moveVertical * speed);
 
         rb.position = new Vector3(
             Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
             rb.position.y,
             Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax));
 
-        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+        rb.AddRelativeTorque(Vector3.up * 6.0f * moveHorizontal);
+
+        rb.maxAngularVelocity = 2;
+
+        //rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
     }
 }
